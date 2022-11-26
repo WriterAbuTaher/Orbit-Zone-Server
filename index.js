e6c40503b6571f315cdaf7e8f04551b4f8513361
Abu Taher <writerabutaher@gmail.com>
2022-11-26 10:41:42 +0600

    server started

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
